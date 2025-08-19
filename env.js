6e6070961755796f0a23a32699bbe5c9cc5c891a
Leonardo Maier <81820588+LeonardoMaierS@users.noreply.github.com>
2025-08-19 09:56:38 -0300

    test

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.ENV = {
-    VERSION: '0.8.0',
-    LAST_UPDATE: '11-08-2025',
+    VERSION: '0.8.1',
+    LAST_UPDATE: '19-08-2025',
     REMOTE_BASE_URL: "https://dashboard-backend-800375288267.us-central1.run.app"
 };
